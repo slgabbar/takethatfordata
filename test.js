fb62e0be70a1207d247e23a7ba215c0ce8ae7400
Michael <michaelyang380@gmail.com>
2018-02-02 10:05:00 -0800

    account creation test firebasE

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
+src="https://www.gstatic.com/firebasejs/4.9.0/firebase.js"
 
-var firebase = require("firebase");
-var firebaseui = require("firebaseui");
-// Initialize firebase
+// Initialize Firebase
+  // TODO: Replace with your project's customized code snippet
   var config = ***REMOVED***
     apiKey: "AIzaSyDmi6qgpfnoCZ8a2FM2APfX74dXfiJ9PFY",
     authDomain: "takethatfordata-8f719.firebaseapp.com",
@@ -10,7 +10,12 @@ var firebaseui = require("firebaseui");
     storageBucket: "takethatfordata-8f719.appspot.com",
     messagingSenderId: "211430446462"
   ***REMOVED***;
-firebase.initializeApp(config);
-const db = firebase.database(),
+  firebase.initializeApp(config);
+  const db = firebase.database(),
       auth = firebase.auth();
-	 
+
+var x = document.getElementById("fname").value;
+var y = document.getElementById("lname").value;
+
+console.log(x);
+console.log(y);
