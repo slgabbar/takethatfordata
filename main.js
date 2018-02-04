80aec95d487602fc4d116d6cab0b31625fc5645f
ejlouie <ejlouie@ucsc.edu>
2018-02-04 14:07:10 -0800

    index.html, main.js
    Simple js function to read, store, and print coordinates

---
+++ main.js
@@ -0,0 +1,33 @@
+var coords = [];
+
+function main() {
+	console.log("We runnin");
+}
+
+function showCoords(event) {
+    var cX = event.clientX;
+    var sX = event.screenX;
+    var cY = event.clientY;
+    var sY = event.screenY;
+    var coords1 = "client - X: " + cX + ", Y coords: " + cY;
+    //var coords2 = "screen - X: " + sX + ", Y coords: " + sY;
+    storeCoordinates(cX, cY);
+    //coords.push({x:cX, y:cY});
+    console.log(coords1);
+	//console.log(coords2);
+
+}
+
+function storeCoordinates(x, y) {
+	var d3_x = x/10;
+	var d3_y = y/10;
+	coords.push({x:d3_x, y:d3_y});
+}
+
+function printArray(event) {
+	for (var i = 0; i < coords.length; i++) {
+		console.log("x: "+coords[i].x+" "+"y: "+coords[i].y);
+	}
+}
+
+$(document).ready(main);
